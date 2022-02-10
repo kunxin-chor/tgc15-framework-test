9d856e583a40295b4b543a155433e4d01458f41b
kunxin-chor <chorkunxin@yahoo.com>
2022-02-10 12:00:13 +0000

    Added index.hbs for products

--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,25 @@
 const express = require('express');
+// an Express router can store routes
 const router = express.Router();
 
 // import in the model
 const { Product } = require('../models');
 
+// add routes to the routers
 router.get('/', async function(req,res){
     // If we are referring to the Model itself,
     // we are referring to the table
-    let products = await Product.collection().fetch();
-    res.send(products.toJSON());
+    let products = await Product.collection().fetch(); // => select * from products
+    
+    res.render('products/index',{
+        'products': products.toJSON() // impt! make sure to call .toJSON() on
+                                      // the results
+    })
 })
 
 router.get('/create', function(req,res){
     res.send("Creating a new product");
 })
 
+// export the router
 module.exports = router;
