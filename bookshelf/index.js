e992b3c9dac8dd82967384b37f6b3d2e249ff703
kunxin-chor <chorkunxin@yahoo.com>
2022-02-19 08:43:30 +0000

    Added reading db settings from .env

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,9 +1,9 @@
 const knex = require('knex')({
-    "client":"mysql",
+    "client":process.env.DB_DRIVER,
     "connection": {
-        "user":"foo",
-        "password":"bar",
-        "database":"organic"
+        "user":process.env.DB_USER,
+        "password":process.env.DB_PASSWORD,
+        "database":process.env.DB_DATABASE
     }
 })
 
