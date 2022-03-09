005f6297492d634c55aee8894a9404d21c612ceb
kunxin-chor <chorkunxin@yahoo.com>
2022-03-09 13:33:13 +0000

    Added pg and db-mirgate-pg

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -3,7 +3,11 @@ const knex = require('knex')({
     "connection": {
         "user":process.env.DB_USER,
         "password":process.env.DB_PASSWORD,
-        "database":process.env.DB_DATABASE
+        "database":process.env.DB_DATABASE,
+        "host":process.env.DB_HOST,
+        "ssl":{
+            'rejectUnauthorized':false
+        }
     }
 })
 
